9ee9ed1297200b871937df43f27e735e0df2897f
Brayden Buchner <buchner.brayden@gmail.com>
2024-08-07 13:13:26 -0400

    fixed addOdd to be iterative

--- Add_Odd.asm
+++ Add_Odd.asm
@@ -10,14 +10,66 @@
 ;
 ; Postcondition:        Register B contains the sum of the odd digits
 
-Add_Odd
-        ldaa        0,x    ; load register a with number in position 1, x
-        adda        0,x         ; Multiply a by 2
-        ldab        2,x    ; load register b with number in position 3, x
-        addb        2,x         ; Multiply b by 2
-        aba                ; Add register a to register b
-        tfr     a,b
-        
-        rts                ; Sum of Odd Digit returned
+oddSum         db      0
+currentOdd     ds      1
+number_Digits  ds      2
 
-        end
+Add_Odd
+
+                stab   number_Digits+1  ; Store number of digits passed to subroutine
+                ldy    #0               ; Init count to 0
+
+
+Push_Odds       ; For i = 0, i less than number digits, i++
+
+                ; Extract even digit and push
+                ldaa    0,x             ; Load current digit into a
+                psha                    ; Push digit to stack
+                inx                     ; Increment x to start from next odd digit
+                inx
+
+                ; i++
+                iny                     ; Increment count
+                iny
+
+                ; i < numDigits
+                cpy     number_Digits   ; Test if this was the last digit
+                blo     Push_Odds       ; Loop if less than numDigits
+
+Pull_Odds      	; While i is greater or equal to 0
+              	pula                    ; Pull the even digit
+
+               	; If this number has two digits
+               	cmpa     #9
+               	bgt      Cross_Multiply ; Cross multiply
+Cross_Multiply
+                ; Do cross multiplication
+                
+                bra     End_Multiply
+                ; else
+                tfr a,b                 ; copy register a to b
+                aba                     ; "multiply a by 2"
+
+End_Multiply
+
+              	; Add to previous sum
+                ldab        oddSum     ; load previous sum
+                aba                    ; Add previous sum to current digit
+                staa    oddSum         ; Store current sum
+                dey                    ; Decrement count
+                dey
+                cpy     #0             ; Test if this was the last digit
+                bhi     Pull_Odds      ; Branch if there are more digits
+
+                ; Return value
+                clra
+                ldab    oddSum
+
+                rts                     ; Sum of Even Digits returned
+
+                end
+
+
+; ---------------------------------
+;        END of Add_Even -
+;----------------------------------
